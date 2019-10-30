a9d5db7a5f54ad7f93bc34a01ad9eeb2a99e6a5c
Aurorans Solis <primalucegd@gmail.com>
2019-10-29 23:16:06 -0700

    Removed unnecessary code and comments from main.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate ahash;
 extern crate num_cpus;
+extern crate rayon;
 
 mod extras;
 mod game;
@@ -25,29 +26,13 @@ use methods::{
     v_eq,
     v_hs_eq
 };
+use solvers::multithreaded_method;
 
 pub const DESIRED_SCORE: usize = 60;
 
 fn main() {
-    // Aight, so an explanation. I'm going to ignore the `Arc<Mutex<>>` part since that's just
-    // making multithreaded mutability safe. Onto the `Vec<Vec<Hashmap<Set>>>`, then. So what I'm
-    // doing with this is storing each unique gamestate I encounter, and then "sorting" those by
-    // length. So gamestates[0] contains all the gamestates with 1 move I've encountered,
-    // gamestates[1] contains all the gamestates with 2 moves I've encountered, and so on. This is
-    // so that I can avoid going down a branch of recursion if I've already found all of its ends.
-    // And from how my recursive algorithm works, if I encounter a complete set of moves again then
-    // I know that I've already tried all of the remaining possibilities for that gamestate.
-    // let gamestates = vec![Vec::new(); DESIRED_SCORE];
-    /*let method = Method::new(v_hs_eq, store_hs, store_npm_s_p, cmp_hpm, spm_intoiter_rev);
-    let mut gamestates = vec![Vec::new(); DESIRED_SCORE];
-    let try_best = STARTING_POINTS
-        .iter().take(1)
-        .map(|&point| base(&mut gamestates, Game::new(), point, method))
-        .next();*/
-    /*let try_best = STARTING_POINTS
-        .par_iter()
-        .map(|&point| base_highest_set(gamestates.clone(), Game::new(), point))
-        .find_first(|game| game.score() >= DESIRED_SCORE);*/
+    let method = Method::new(v_hs_eq, store_hs, store_npm_s_p, cmp_hpm, spm_intoiter_rev);
+    let try_best = multithreaded_method(method);
     if let Some(best) = try_best {
         println!("Got: {}", best.score());
         for set in best.sets.iter() {
